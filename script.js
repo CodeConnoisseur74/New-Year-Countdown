43fbacf377505697483b2a78e91099cb4727cfc2
CodeConnoisseur74 <sherry.andrews@outlook.com>
2022-12-31 00:20:39 +0100

    add parameters to update the countdown function.

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear +1} 00:00:00`);
+
+function updateCountdown() {
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+
+}
+
+
+setInterval(updateCountdown, 1000);
